941d402dfcbdc2b98d97e6924dc2ce87636c323e
Aldo Gonzalez <aldogonjuarez@.com>
2024-01-18 13:25:19 -0600

    create custom hook

---
+++ src/hooks/useDataFetching.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const useDataFetching = (dataSource) => {
+
+    const [ loading, setLoading ] = useState(false);
+    const [ data, setData ] = useState([]);
+    const [ error, setError ] = useState('');
+
+    useEffect(() => {
+         const fetchData = async () => {
+            try {
+                const data = await fetch(dataSource);
+                const result = await data.json();
+                if(result){
+                    setData(result);
+                    setLoading(false);
+                }
+            } catch (error) {
+                setLoading(false);
+                setError(error.message);
+            }
+        }
+    }, dataSource)
+
+    return[loading, error, data];
+}
+
+export default useDataFetching;
